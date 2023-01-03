a69c38fe611583d312d7fb96f1baa544993ab00c
Om Ganesh <dreamcodelife@gmail.com>
2023-01-03 18:08:05 -0500

    Create template for VisualStudio C# Solution

---
+++ NYCSolution/ArrayProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, NYC 2023 Practice Session!");
+        }
+    }
+}
